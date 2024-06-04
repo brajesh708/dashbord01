1fb0bc18ab574ccd28e877a6dff74b7a7b18689e
brajesh mewada <154889477+brajesh708@users.noreply.github.com>
2024-06-04 16:24:33 +0530

    Add files via upload

---
+++ stri.py
@@ -0,0 +1,23 @@
+# str="brajesh"
+# print(str)
+
+# str1 ="neeeraj"
+# print(str1)
+# print(type(str1))
+
+
+# a="name"
+# print(type(a))
+
+
+# a= ""raj""
+# print(a)
+
+# b="ram"
+# print(b)
+for i in range(10):
+    if i%2==0:
+        continue
+    print(i)
+
+
